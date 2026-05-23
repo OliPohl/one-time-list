60a9bfa455e5a53c10df31a19bc164fe0bb0910f
OliPohl <oliver@pohl66.de>
2026-05-23 16:03:30 +0200

    Add TextInput with a scaling textarea

---
+++ src/lib/TaskInput.svelte
@@ -0,0 +1,70 @@
+<!-- src/lib/TaskInput.svelte -->
+<script>
+  let text = $state('');
+
+  /**
+	 * @param {{ target: any; }} event
+	 */
+  function handleInput(event) {
+    const textarea = event.target;
+    
+    textarea.style.height = 'auto';
+    const maxPixelHeight = 18 * 1.5 * 5; 
+    
+    if (textarea.scrollHeight > maxPixelHeight) {
+      textarea.style.height = `${maxPixelHeight}px`;
+    } else {
+      textarea.style.height = `${textarea.scrollHeight - 4}px`;
+    }
+  }
+</script>
+
+
+<div class="wrapper">
+  <div class="container">
+    <textarea 
+    name="text" 
+    rows=1
+    wrap="soft" 
+    placeholder="What needs to be done?"
+    class="input-field"
+    maxlength="500"
+    bind:value={text}
+    oninput={handleInput}
+    ></textarea>
+    <span class="send-btn">x</span>
+  </div>
+</div>
+
+
+<style>
+  .wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: rgba(255, 255, 255, 0.055);
+  }
+
+  .container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    background-color: rgb(69, 69, 69);
+    padding: 15px 25px;
+    border-radius: 40px;
+  }
+
+  .input-field {
+    flex-grow: 1;
+    font-size: 18px;
+    line-height: 1.5;
+    background-color: transparent;
+    border-color: transparent;
+    resize: none;
+    overflow:scroll;
+    color: #bebebe;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- src/routes/+page.svelte -->
+<script>
+  import { TaskInput } from '$lib';
+</script>
+
+<main class="page-layout">
+  <TaskInput />
+</main>
+
+<style>
+  .page-layout {
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    padding: 0 10px;
+
+    overflow-y: scroll;
+    overflow-x: hidden;
+
+    max-width: 600px;
+  }
+</style>
